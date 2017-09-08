ade24cd1ee70cdb6dcfe2c1556658d9891088b13
Pat Tochit <mmpattt.t@gmail.com>
2017-09-08 18:54:15 +0700

    add js

---
+++ js/getbeer.js
@@ -0,0 +1,58 @@
+
+let getBeerAPI = function getBeerAPI(name) {
+    return fetch('https://api.punkapi.com/v2/beers/?beer_name=' + name).then(function (response) {
+        return response.json();
+    }).catch(function (e) {
+        return console.err('Fail');
+    }).then(function (response) {
+        let result = "";
+        document.getElementById("num").innerHTML = response.length;
+        for (let i = 0; i < response.length; i++) {
+            result += "<div class='col-lg-4'><b>Name:</b>" + response[i].name+"<br>" + '<img src="' + response[i].image_url + '" width="80" height="150">' +"<br><b>Tagname:</b>" + response[i].tagline+"<br><b>First brewed:</b>"+ response[i].first_brewed+"<br><b>Yeast:</b>"+ response[i].ingredients.yeast+ "<hr></div>";
+        }
+        document.getElementById("beers").innerHTML = result;
+    });
+  };
+
+  let getYeast = function getYeast(name) {
+    return fetch('https://api.punkapi.com/v2/beers/?yeast=' + name).then(function (response) {
+        return response.json();
+    }).catch(function (e) {
+        return console.err('Fail');
+    }).then(function (response) {
+        let result = "";
+        document.getElementById("num").innerHTML = response.length;
+        for (let i = 0; i < response.length; i++) {
+            result += "<div class='col-lg-4'><b>Name:</b>" + response[i].name+"<br>" + '<img src="' + response[i].image_url + '" width="80" height="150">' +"<br><b>Tagname:</b>" + response[i].tagline+"<br><b>First brewed:</b>"+ response[i].first_brewed+"<br><b>Yeast:</b>"+ response[i].ingredients.yeast+ "<hr></div>";
+        }
+        document.getElementById("beers").innerHTML = result;
+    });
+  };
+
+  let getBeerYeast = function getBeerYeast(name,yeast) {
+    return fetch('https://api.punkapi.com/v2/beers/?beer_name=' + name + '&?yeast=' + yeast).then(function (response) {
+        return response.json();
+    }).catch(function (e) {
+        return console.err('Fail');
+    }).then(function (response) {
+        var result = "";
+        document.getElementById("num").innerHTML = response.length;
+        for (let i = 0; i < response.length; i++) {
+            result += "<div class='col-lg-4'><b>Name:</b>" + response[i].name+"<br>" + '<img src="' + response[i].image_url + '" width="80" height="150">' +"<br><b>Tagname:</b>" + response[i].tagline+"<br><b>First brewed:</b>"+ response[i].first_brewed+"<br><b>Yeast:</b>"+ response[i].ingredients.yeast+ "<hr></div>";
+        }
+        document.getElementById("beers").innerHTML = result;
+  
+    });
+  };
+
+  var findBeer = function findBeer(name,yeast){
+    if (yeast != "" && name != "") {
+        getBeerYeast(name,yeast);
+    } else if (yeast != "") {
+        getYeast(yeast);
+    } else if (name != "") {
+        getBeerAPI(name);
+    } else {
+        location.reload();
+    }
+  }
---
+++ js/index.js
@@ -0,0 +1,15 @@
+import { getBeers } from './api.js'
+
+getBeers()
+  .then(response => response.json())
+  .catch(e => console.error('Something went wrong', e))
+  .then(function(response) {
+    document.getElementById("num").innerHTML += response.length;
+    let result = "";
+    for (let i = 0; i < response.length; i++) {
+      result += "<div class='col-lg-4 border border-warning'><b>Name:</b>" + response[i].name+"<br>" + '<img src="' + response[i].image_url + '" width="80" height="150">' +"<br><b>Tagname:</b>" + response[i].tagline+"<br><b>First brewed:</b>"+ response[i].first_brewed+"<br><b>Yeast:</b>"+ response[i].ingredients.yeast+ "<hr></div>";
+    }
+    document.getElementById("beers").innerHTML = result;
+  })
+
+  
